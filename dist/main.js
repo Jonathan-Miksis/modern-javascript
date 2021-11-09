acd7b51a6f6bad464f21cf99116ae370a7d890d2
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-08 22:26:02 -0500

    add babble to ensure js can run in all browsers

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \******************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("console.log(\"Hello from JS!\");\n\nvar moment = __webpack_require__(/*! moment */ \"./node_modules/moment/moment.js\");\n\nconsole.log(moment().format('MMMM Do YYYY, h:mm:ss a'));\n\n//# sourceURL=webpack://modern-javascript/./index.js?");
+eval("console.log(\"Hello from JS!\");\n\nvar moment = __webpack_require__(/*! moment */ \"./node_modules/moment/moment.js\");\n\nconsole.log(moment().format('MMMM Do YYYY, h:mm:ss a'));\nvar name = \"Bob\",\n    time = \"today\";\nconsole.log(\"Hello \".concat(name, \", how are you \").concat(time, \"?\"));\n\n//# sourceURL=webpack://modern-javascript/./index.js?");
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ console.log("Hello from JS!");
 var moment = require('moment');
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
